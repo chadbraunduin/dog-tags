3c6464a5a5caa4790bf67b301c199d490760db95
Chad Braun-Duin <chadbraunduin@gmail.com>
2010-07-24 12:52:22 -0400

    Stable version 1.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject dog-tags "1.1.0-SNAPSHOT"
+(defproject dog-tags "1.1.0"
   :description "Create a tag cloud or html list of tags. Written in Clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [hiccup "0.2.3"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
